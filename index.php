b30540c38634f87739216c349f50f89dee014729
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-06-19 15:09:55 -0400

    fetch xml

--- index.php
+++ index.php
@@ -11,7 +11,20 @@ use Silex\Application;
 $app = require __DIR__.'/bootstrap.php';
 
 $app->get('/', function(Application $app){
-  return new Response("Hola");
+  $xml = simplexml_load_file("http://jreutter.sitios.ing.uc.cl/alias.xml");
+  $result = $xml->xpath("//tr[position()>1]/td[1]");
+  $documents = array();
+  foreach ($result as $alias) {
+    $url = "http://query17-8.ing.puc.cl/wordInContent?keyword=" + $alias[0];
+    $ch = curl_init($url);
+    $json = curl_exec($ch);
+    $documents[$alias[0]] = json_decode($json);
+    curl_close($ch);
+  }
+  return $app['twig']->render("index.html.twig", [
+    'aliases' => $result,
+    'messages' => $documents
+  ]);
 })->bind('index');
 
 $app->get('/api', function(Application $app){
